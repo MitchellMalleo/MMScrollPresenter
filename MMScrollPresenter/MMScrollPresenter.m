5826c0e0a7725f8b8ea054fa69a9110fad5dcbd5
nwpuhmz <scuhmz@gmail.com>
2015-04-13 20:14:27 +0800

    Update MMScrollPresenter.m
    make sure the  titleScrollView can not drag .

--- MMScrollPresenter/MMScrollPresenter.m
+++ MMScrollPresenter/MMScrollPresenter.m
@@ -64,8 +64,8 @@ static int const pageViewPadding = 20;
 - (void)setupViews
 {
     self.titleScrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, self.frame.size.height - titleViewHeight, self.frame.size.width * [self.pageArray count], titleViewHeight)];
+    [self.titleScrollView setScrollEnabled:NO];
     [self addSubview:self.titleScrollView];
-    
     int pageArrayIndex = 0;
     
     for (MMScrollPage *page in self.pageArray)
